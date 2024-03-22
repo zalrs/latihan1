b1b25e20a1b7cf1f18fad9f778fa90e5f172406f
zalrs <faizalar1603@gmail.com>
2024-03-22 15:47:30 +0700

    jaskdsa

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Faizal Aflah Rizaldi
+    Saya tingga di daerah Petukangan Utara
+    olah raga yang saya sukai adalah
+    Bulutangkis";
+ }
+}
---
+++ latihan.php
@@ -0,0 +1,8 @@
+<?php
+class Latihan extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan ksadksadk</h1>";
+ }
+}
